4940ed7f47ae62a63a0537154ae7bd06a7052e00
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-10-29 00:08:51 +0600

    error solve

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = require(express());
+const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
